bfd16f09ae30189e69e74c26ea801d34c3096fb2
LucaCorleto6 <lcorleto@mail.austral.edu.ar>
2026-03-27 10:48:36 -0300

    Update input_calc.py

--- input_calc.py
+++ input_calc.py
@@ -6,7 +6,7 @@ def rectangle():
     base = int(input())
     altura = int(input())
 
-    print(f'Base: {base}')
-    print(f'Altura: {altura}')
+    print(f"Base: {base}")
+    print(f"Altura: {altura}")
     print(f"Area: {base * altura}")
     print(f"Perimetro: {2*base + 2*altura}")
