bd9b86e2fe1e1d7e94b2b24cb8ea768c02955b0a
LucaCorleto6 <lcorleto@mail.austral.edu.ar>
2026-03-27 10:22:00 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -25,3 +25,42 @@ def ficha():
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
     pass
+    # - Inputs
+    nombre_completo = input().lower()
+    email = input().lower()
+    nota1 = int(input())
+    nota2 = int(input())
+    nota3 = int(input())
+
+    # - Funciones
+
+    nombre_limpio = nombre_completo.strip().title()
+    espacio_nombre = nombre_limpio.find(" ")
+    solo_nombre = nombre_limpio[:espacio_nombre]
+    apellido = nombre_limpio[espacio_nombre+1:]
+    dominio = email[email.find("@")+1:]
+    suma = nota1 + nota2 + nota3
+    promedio = suma/3
+
+    # - Prints
+    print("=" * 24)
+    print('    FICHA DEL ALUMNO')
+    print("=" * 24)
+    print(f"Nombre: {nombre_limpio}")
+    print(f"Email: {email}")
+    print(f"Caracteres en nombre: {len(nombre_limpio)}")
+    print(f"Iniciales: {nombre_limpio[0]}{nombre_limpio[(espacio_nombre + 1)]}")
+    print(f"Usuario: {apellido.lower()}.{solo_nombre.lower()}")
+    print(f"Email valido: {"@" in email}")
+    print(f"Dominio: {dominio}")
+    print(f"Nombre para archivo: {nombre_limpio.replace(" ","_")}")
+    print(f"Cantidad de a: {nombre_limpio.count("a")}")
+    print(f"Codigo secreto: {nombre_limpio[::-1].upper()}")
+    print(f"Nota 1: {nota1}")
+    print(f"Nota 2: {nota2}")
+    print(f"Nota 3: {nota3}")
+    print(f"Suma: {suma}")
+    print(f"Promedio: {float(promedio)}")
+    print(f"Promedio entero: {int(promedio)}")
+    print("=" * 24)
+
